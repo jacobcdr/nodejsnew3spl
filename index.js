9431e7bbec0bc9e494ba66a34060b1b63cef79cd
jacobcdr <112586026+jacobcdr@users.noreply.github.com>
2024-01-19 13:08:38 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const http = require('http');
+const fs = require('fs');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+  fs.readFile('index.html', (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'text/plain');
+      res.end('Error loading the page');
+    } else {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html');
+      res.end(data);
+    }
+  });
 });
 
 server.listen(PORT, () => {
